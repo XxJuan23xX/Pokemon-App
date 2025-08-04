1d68f6ee0f70c306e4f1431c0b64afa9ed3a25de
RosadoMike <rosadomiguel2305@gmail.com>
2025-08-04 12:41:40 -0600

    pepe2

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -64,7 +64,7 @@ const Login = () => {
 
               <div>
                 <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Usuario
+                  Usuari0
                 </label>
                 <input
                   type="text"
